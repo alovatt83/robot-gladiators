08f7cf838b995fde2a1f17991b32b02e21bb3844
Allen <allenlovatt@gmail.com>
2020-12-19 20:43:29 -0500

    added enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,15 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
-var enemyAttack = 101;
+var enemyAttack = 12;
 
-var fight = function() {
-    window.alert("Welcome to Robot Gladiators!");
+var fight = function(enemyName) {
+    while(enemyHealth > 0) {
+   
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 enemyHealth = enemyHealth - playerAttack;
   // Log a resulting message to the console so we know that it worked.
@@ -36,5 +38,7 @@ if (playerHealth <= 0) {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
 };
-
-fight();
+}
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
